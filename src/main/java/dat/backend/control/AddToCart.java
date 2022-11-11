86127e63b076d1f1f4d3b1d4210f012f0aaa8bbb
FeliciaFavrholdt <feliciafavrholdt93@gmail.com>
2022-11-11 01:47:09 +0100

    adds shoppingcart and cupcake entities

--- src/main/java/dat/backend/control/AddToCart.java
+++ src/main/java/dat/backend/control/AddToCart.java
@@ -1,5 +1,8 @@
 package dat.backend.control;
 
+import dat.backend.model.entities.Cupcake;
+import dat.backend.model.entities.ShoppingCart;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
@@ -13,6 +16,18 @@ public class AddToCart extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        ShoppingCart cart = (ShoppingCart) session.getAttribute("cart");
 
+        int toppingID = Integer.parseInt(request.getParameter("toppings"));
+        int bottomID = Integer.parseInt(request.getParameter("bottoms"));
+        int amount = Integer.parseInt(request.getParameter("amount"));
+
+        Cupcake cupcake = new Cupcake(toppingID, bottomID, amount);
+        cart.add(cupcake);
+        session.setAttribute("cart", cart);
+        request.setAttribute("cartsize",cart.getNumberOfCupcakes());
+
+        request.getRequestDispatcher("shoppingcart.jsp").forward(request,response);
     }
 }
---
+++ src/main/java/dat/backend/model/entities/Cupcake.java
@@ -0,0 +1,38 @@
+package dat.backend.model.entities;
+
+public class Cupcake {
+
+    private int toppingID;
+    private int bottomID;
+    private int quantity;
+
+    public Cupcake(int toppingID, int bottomID, int quantity) {
+        this.toppingID = toppingID;
+        this.bottomID = bottomID;
+        this.quantity = quantity;
+    }
+
+    public int getToppingID() {
+        return toppingID;
+    }
+
+    public void setToppingID(int toppingID) {
+        this.toppingID = toppingID;
+    }
+
+    public int getBottomID() {
+        return bottomID;
+    }
+
+    public void setBottomID(int bottomID) {
+        this.bottomID = bottomID;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
