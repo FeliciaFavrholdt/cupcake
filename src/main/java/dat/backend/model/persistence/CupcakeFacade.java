d4c5a7385dd601cfc58cf18e14f351dcb62bc7d1
FeliciaFavrholdt <feliciafavrholdt93@gmail.com>
2022-11-11 04:53:08 +0100

    adds cupcake facade and mapper

--- src/main/java/dat/backend/model/persistence/CupcakeFacade.java
+++ src/main/java/dat/backend/model/persistence/CupcakeFacade.java
@@ -2,13 +2,14 @@ package dat.backend.model.persistence;
 
 import dat.backend.model.entities.Bottom;
 import dat.backend.model.entities.Topping;
+import dat.backend.model.exceptions.DatabaseException;
 
 public class CupcakeFacade {
-    public static Topping getToppingByID(int toppingID) {
-        return CupcakeMapper.getToppingByID(toppingID);
+    public static Topping getToppingByID(int toppingID, ConnectionPool connectionPool) throws DatabaseException {
+        return CupcakeMapper.getToppingByID(toppingID, connectionPool);
     }
 
-    public static Bottom getBottomByID(int bottomID) {
-        return CupcakeMapper.getBottomID(bottomID);
+    public static Bottom getBottomByID(int bottomID, ConnectionPool connectionPool) throws DatabaseException {
+        return CupcakeMapper.getBottomID(bottomID, connectionPool);
     }
 }
--- src/main/java/dat/backend/model/persistence/CupcakeMapper.java
+++ src/main/java/dat/backend/model/persistence/CupcakeMapper.java
@@ -4,42 +4,41 @@ import dat.backend.model.entities.Bottom;
 import dat.backend.model.entities.Topping;
 import dat.backend.model.entities.User;
 import dat.backend.model.exceptions.DatabaseException;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 class CupcakeMapper {
 
-    static Topping getToppingByID(int toppingID) {
+    static Topping getToppingByID(int toppingID, ConnectionPool connectionPool) throws DatabaseException {
         Logger.getLogger("web").log(Level.INFO, "");
-        Topping topping;
+        Topping topping = null;
 
-        return topping;
-    }
+        String sql = "SELECT * FROM topping WHERE username = ? AND password = ?";
 
-    static Bottom getBottomByID(int bottomID) {
-        Logger.getLogger("web").log(Level.INFO, "");
-        Bottom bottom;
-
-        String sql = "insert into user (username, password, role) values (?,?,?)";
         try (Connection connection = connectionPool.getConnection()) {
             try (PreparedStatement ps = connection.prepareStatement(sql)) {
                 ps.setString(1, username);
                 ps.setString(2, password);
-                ps.setString(3, role);
-                int rowsAffected = ps.executeUpdate();
-                if (rowsAffected == 1) {
-                    bottom = new User(username, password, role);
+                ResultSet rs = ps.executeQuery();
+                if (rs.next()) {
+                    String role = rs.getString("role");
+                    user = new User(username, password, role);
                 } else {
-                    throw new DatabaseException("The user with username = " + username + " could not be inserted into the database");
+                    throw new DatabaseException("Wrong username or password");
                 }
             }
         } catch (SQLException ex) {
-            throw new DatabaseException(ex, "Could not insert username into database");
+            throw new DatabaseException(ex, "Error logging in. Something went wrong with the database");
         }
-        return bottom;
+        return topping;
+    }
+
+
+    public static Bottom getBottomID(int bottomID, ConnectionPool connectionPool) throws DatabaseException {
+        return null;
     }
 }
