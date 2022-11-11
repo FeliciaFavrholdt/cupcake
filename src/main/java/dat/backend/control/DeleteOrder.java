0db3f2aaccf2d7bfd7ff706cfb9a29c929cf42ec
FeliciaFavrholdt <feliciafavrholdt93@gmail.com>
2022-11-11 09:49:22 +0100

    updates order entity and adds methods to add/remove orders

--- src/main/java/dat/backend/control/DeleteOrder.java
+++ src/main/java/dat/backend/control/DeleteOrder.java
@@ -5,7 +5,7 @@ import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 
-@WebServlet(name = "DeleteOrder", value = "/DeleteOrder")
+@WebServlet(name = "DeleteOrder", value = "/deleteorder")
 public class DeleteOrder extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
--- src/main/java/dat/backend/model/entities/Order.java
+++ src/main/java/dat/backend/model/entities/Order.java
@@ -1,11 +1,34 @@
 package dat.backend.model.entities;
 
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Order {
     private int orderID;
     private int customerID;
     private Timestamp created;
+
+    private List<Order> orderList = new ArrayList<>();
+
+    public Order() {
+    }
+
+    public void add(Order order) {
+       orderList.add(order);
+    }
+
+    public void remove(Order order) {
+        orderList.remove(order);
+    }
+
+    public Object getAllOrders() {
+        return orderList.size();
+    }
+
+    public List<Order> getOrderList() {
+        return orderList;
+    }
 
     public Order(int orderID, int customerID, Timestamp created) {
         this.orderID = orderID;
